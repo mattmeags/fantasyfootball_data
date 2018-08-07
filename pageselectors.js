3851c451cb5d156d31e77bdc4d7bd52d98aa7a93
Matt Meagher <mattmeags8@Matts-MacBook-Pro.local>
2018-08-06 23:03:51 -0500

    successful scrape code complete, began modular application of all data tables on site

---
+++ pageselectors.js
@@ -0,0 +1,38 @@
+module.exports = {
+    teamOffense : {
+        url: 'https://www.pro-football-reference.com/years/2017/',
+        menuSelector: '#all_team_stats',
+        csvSelector: '#csv_team_stats',
+        fileName: 'allTeamOffense'
+    },
+    passingOffense : {
+        url: 'https://www.pro-football-reference.com/years/2017/',
+        menuSelector: '#all_passing',
+        csvSelector: '#csv_passing',
+        fileName: 'passingOffense'
+    },
+    rushingOffense : {
+        url: '',
+        menuSelector: '',
+        csvSelector: '',
+        fileName: ''
+    },
+    teamDefense : {
+        url: 'https://www.pro-football-reference.com/years/2017/opp.htm',
+        menuSelector: '#all_team_stats',
+        csvSelector: '#csv_team_stats',
+        fileName: 'allTeamDefense'
+    },
+    passingDefense : {
+        url: 'https://www.pro-football-reference.com/years/2017/opp.htm',
+        menuSelector: '#all_passing',
+        csvSelector: '',
+        fileName: ''
+    },
+    rushingDefense : {
+        url: 'https://www.pro-football-reference.com/years/2017/opp.htm',
+        menuSelector: '',
+        csvSelector: '',
+        fileName: ''
+    }
+}
--- scrape.js
+++ scrape.js
@@ -1,24 +1,54 @@
 const puppeteer = require('puppeteer'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    fs = require('fs-extra');
 
 const url = 'https://www.pro-football-reference.com/years/2017/'
 
-async function run() {
-    const browser = await puppeteer.launch();
+async function scrape() {
+    const browser = await puppeteer.launch({
+        //headless: false
+    });
     const page = await browser.newPage();
 
     await page.goto(url);
-    //await page.waitFor('#csv_team_stats');
 
-    await page.click('#all_team_stats .hasmore li:nth-of-type(4) button');
+    await page.waitFor('#all_team_stats .hasmore li:nth-of-type(4) button');
 
     let html = await page.content();
 
     const $ = cheerio.load(html);
 
-    await console.log($('#all_team_stats .hasmore').html());
+    await page.hover('#all_team_stats .hasmore');
+
+    const tableMenu = await page.$('#all_team_stats .hasmore');
+
+    await tableMenu.hover();
+
+    await page.waitFor('.drophover');
+
+    const csvBtn = await page.$('#all_team_stats .hasmore li:nth-of-type(4) button');
+
+    await csvBtn.click();
+
+    const content = await page.$('.table_outer_container');;
+
+    await page.waitFor('#csv_team_stats');
+
+    let text = await page.evaluate(() => document.querySelector('#csv_team_stats').innerHTML);
+    text = text.slice(19, -1).trim();
+    console.log(text);
 
     await browser.close();
+
+    await writeCSVFiles(text);
 }
 
-run();
+async function writeCSVFiles(data) {
+    try {
+        fs.outputFile('./data_files/allTeamOffense.csv', data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+scrape();
